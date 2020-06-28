eaf87490b6e9afb91f654fa13ce243bd4526e5f2
mugg84 <56695670+mugg84@users.noreply.github.com>
2020-06-28 19:23:45 +0100

    project completed

---
+++ src/Components/Previewer.js
@@ -0,0 +1,20 @@
+import React from "react";
+import marked from "marked";
+
+const Previewer = ({ text }) => {
+  const getMarkdownText = () => {
+    let rawMarkup = marked(text, { sanitize: true });
+    return { __html: rawMarkup };
+  };
+
+  return (
+    <div className="preview-holder">
+      <header>
+        <h2>Preview</h2>
+      </header>
+      <div id="preview" dangerouslySetInnerHTML={getMarkdownText()}></div>
+    </div>
+  );
+};
+
+export default Previewer;
